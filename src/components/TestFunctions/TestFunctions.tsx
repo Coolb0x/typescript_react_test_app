06075b76b46cb0e65c8296f7e47b239e44ee4218
Coolb0x <45721224+Coolb0x@users.noreply.github.com>
2024-12-27 23:50:24 +0200

    Updated TestFunctions.tsx
    Added a test function with a few variable types

---
+++ src/components/TestFunctions/TestFunctions.tsx
@@ -0,0 +1,21 @@
+export default function TestFunctions() {
+  const number1 = 5;
+  const number2 = 10;
+  const showResult = true;
+  const errorString = "The result is: ";
+
+  const sum = (a: number, b: number, ifShow: boolean, ifError: string): number | void => {
+    return ifShow ? a + b : console.log(ifError);
+  };
+
+  const result = sum(number1, number2, showResult, errorString);
+
+  return (
+    <>
+      <div>
+        <h3>Test Functions</h3>
+        <p>{`The sum of ${number1} and ${number2} is ${result}`}</p>
+      </div>
+    </>
+  );
+}
