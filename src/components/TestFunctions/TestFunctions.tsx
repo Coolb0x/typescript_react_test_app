852888cf4c51ed865f2eb52eee1ec243a4c9879d
Coolb0x <45721224+Coolb0x@users.noreply.github.com>
2024-12-28 00:58:40 +0200

    Update TestFunctions.tsx
    Added a TS object

--- src/components/TestFunctions/TestFunctions.tsx
+++ src/components/TestFunctions/TestFunctions.tsx
@@ -3,6 +3,13 @@ export default function TestFunctions() {
   const number2 = 10;
   const showResult = true;
   const errorString = "The result is: ";
+  const person: {
+    name: string;
+    age: number;
+  } = {
+    name: "Max",
+    age: 30,
+  };
 
   const sum = (a: number, b: number, ifShow: boolean, ifError: string): number | void => {
     return ifShow ? a + b : console.log(ifError);
@@ -15,6 +22,9 @@ export default function TestFunctions() {
       <div>
         <h3>Test Functions</h3>
         <p>{`The sum of ${number1} and ${number2} is ${result}`}</p>
+        <p>
+          This is {person.name} and he is {person.age} years{" "}
+        </p>
       </div>
     </>
   );
