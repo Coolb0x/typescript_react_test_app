eacd24e7ddf69b0114431830fc91c1bb507ffbd6
Coolb0x <45721224+Coolb0x@users.noreply.github.com>
2025-01-03 16:13:28 +0200

    Updated AdvancedTypes
    Moved code to AdvancedTypes

--- src/components/AdvancedTypes/AdvancedTypes.tsx
+++ src/components/AdvancedTypes/AdvancedTypes.tsx
@@ -1,4 +1,5 @@
-// Intersection Types
+export default function AdvancedTypes() {
+    // Intersection Types
 
 // Or we can do it with interfaces
 
@@ -7,98 +8,142 @@
 // interface ElevatedEmployee extends Admin, Employee
 
 type Admin = {
-  name: string;
-  privileges: string[];
-};
-
-type Employee = {
-  name: string;
-  startDate: Date;
-};
-
-type ElevatedEmployee = Admin & Employee;
-
-const emp1: ElevatedEmployee = {
-  name: "Coolbox",
-  privileges: ["create-server"],
-  startDate: new Date(),
-};
-
-// Type Gurads
-type Combinable = string | number;
-
-const simpleAdd = (a: Combinable, b: Combinable) => {
-  if (typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString();
+    name: string;
+    privileges: string[];
+  };
+  
+  type Employee = {
+    name: string;
+    startDate: Date;
+  };
+  
+  type ElevatedEmployee = Admin & Employee;
+  
+  const emp1: ElevatedEmployee = {
+    name: "Coolbox",
+    privileges: ["create-server"],
+    startDate: new Date(),
+  };
+  
+  // Type Gurads
+  type Combinable = string | number;
+  
+  const simpleAdd = (a: Combinable, b: Combinable) => {
+    if (typeof a === "string" || typeof b === "string") {
+      return a.toString() + b.toString();
+    }
+    return a + b;
+  };
+  
+  type UnknownEmployee = Employee | Admin;
+  
+  // Type Gurads with "in" keyword
+  const printEmployeeInformation = (emp: UnknownEmployee) => {
+    console.log(`Name: ${emp.name}`);
+  
+    if ("privileges" in emp) {
+      console.log(`Privileges: ${emp.privileges}`);
+    }
+    if ("startDate" in emp) {
+      console.log(`Start Date: ${emp.startDate}`);
+    }
+  };
+  
+  // Type Gurads with "instanceof" keyword
+  
+  class Car {
+    drive() {
+      console.log("Driving a car...");
+    }
   }
-  return a + b;
-};
-
-type UnknownEmployee = Employee | Admin;
-
-// Type Gurads with "in" keyword
-const printEmployeeInformation = (emp: UnknownEmployee) => {
-  console.log(`Name: ${emp.name}`);
-
-  if ("privileges" in emp) {
-    console.log(`Privileges: ${emp.privileges}`);
+  
+  class Truck {
+  
+    loadCargo(amount: number) {
+      console.log(`Loading cargo... ${amount}`);
+    }
   }
-  if ("startDate" in emp) {
-    console.log(`Start Date: ${emp.startDate}`);
+  
+  type Vehicle = Car | Truck;
+  
+  const transport (vehicle: Vehicle) => {
+    if (vehicle instanceof Car) {
+      vehicle.drive();
+    } else {
+      vehicle.loadCargo(1000);
+    }
   }
-};
-
-// Type Gurads with "instanceof" keyword
-
-class Car {
-  drive() {
-    console.log("Driving a car...");
+  
+  // Discriminated Unions
+  
+  interface Bird {
+    type: "bird";
+    flyingSpeed: number;
   }
+  
+  interface Horse {
+    type: "horse";
+    runningSpeed: number;
+  }
+  
+  type Animal = Bird | Horse;
+  
+  const moveAnimal = (animal: Animal) => {
+    let speed;
+    switch (animal.type) {
+      case "bird":
+        speed = animal.flyingSpeed;
+        break;
+      case "horse":
+        speed = animal.runningSpeed;
+        break;
+    }
+    console.log(`Moving at speed: ${speed}`);
+  };
+  // Type Casting
+  
+  const inputElement = document.getElementById("input") as HTMLInputElement;
+  
+    // Index Properties
+    interface ErrorContainer {
+      [prop: string]: string;
+    }
+  
+    // Function Overloads
+  
+    type Combinable = string | number;
+  
+    const a: Combinable;
+    const b: Combinable;
+  
+    function add(a: number, b: number): number;
+    function add(a: string, b: string): string;
+    const add = (a: Combinable, b: Combinable) => {
+      return typeof a === "string" || typeof b === "string" ? a.toString() + b.toString() : a + b;
+    };
+  
+    const result = add("Anton", "T");
+  
+    result.split(" ");
+  
+    // Chaining Methods
+  
+    const userData = {
+      id: "u1",
+      name: "Anton",
+      job: { title: "CEO", description: "My own company" },
+    };
+  
+    console.log(userData.job?.description, userData.job?.title);
+  
+    // NUllish Coalescing with "??" operator
+  
+    const userInput = null;
+    const storedData = userInput ?? "DEFAULT";
+  
+
+    return
+    <>
+    <h3>Advaned Types Component</h3>
+    </>
 }
-
-class Truck {
-
-  loadCargo(amount: number) {
-    console.log(`Loading cargo... ${amount}`);
-  }
-}
-
-type Vehicle = Car | Truck;
-
-const transport (vehicle: Vehicle) => {
-  if (vehicle instanceof Car) {
-    vehicle.drive();
-  } else {
-    vehicle.loadCargo(1000);
-  }
-}
-
-// Discriminated Unions
-
-interface Bird {
-  type: "bird";
-  flyingSpeed: number;
-}
-
-interface Horse {
-  type: "horse";
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-const moveAnimal = (animal: Animal) => {
-  let speed;
-  switch (animal.type) {
-    case "bird":
-      speed = animal.flyingSpeed;
-      break;
-    case "horse":
-      speed = animal.runningSpeed;
-      break;
-  }
-  console.log(`Moving at speed: ${speed}`);
-};
-// Type Casting
-
-const inputElement = document.getElementById("input") as HTMLInputElement;
--- src/components/TestFunctions/TestFunctions.tsx
+++ src/components/TestFunctions/TestFunctions.tsx
@@ -4,6 +4,38 @@ export default function TestFunctions() {
     [prop: string]: string;
   }
 
+  // Function Overloads
+
+  type Combinable = string | number;
+
+  const a: Combinable;
+  const b: Combinable;
+
+  function add(a: number, b: number): number;
+  function add(a: string, b: string): string;
+  const add = (a: Combinable, b: Combinable) => {
+    return typeof a === "string" || typeof b === "string" ? a.toString() + b.toString() : a + b;
+  };
+
+  const result = add("Anton", "T");
+
+  result.split(" ");
+
+  // Chaining Methods
+
+  const userData = {
+    id: "u1",
+    name: "Anton",
+    job: { title: "CEO", description: "My own company" },
+  };
+
+  console.log(userData.job?.description, userData.job?.title);
+
+  // NUllish Coalescing with "??" operator
+
+  const userInput = null;
+  const storedData = userInput ?? "DEFAULT";
+
   return (
     <>
       <div>
