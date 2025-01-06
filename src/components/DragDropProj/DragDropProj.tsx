b5a88950203f1cfb188be7b1d7bcf79af737c3de
Coolb0x <45721224+Coolb0x@users.noreply.github.com>
2025-01-06 22:02:45 +0200

    Tailwind config
    Tailwind config

--- src/components/DragDropProj/DragDropProj.tsx
+++ src/components/DragDropProj/DragDropProj.tsx
@@ -1,9 +1,33 @@
+// Check instructions https://react-code-block.netlify.app/usage
+
+import { CodeBlock } from "react-code-block";
+
 export default function DragDropProj() {
+  const code: string = `
+    const welcomeMessage = "Hello,";
+    let target = "world!";
+    
+    function greet(target) {
+      console.log(welcomeMessage + " " + target);
+    }
+    
+    greet(target);
+    
+    for (let i = 0; i < 5; i++) {
+      console.log("Count: {i}");
+    }
+    `;
+  const language: string = "js";
+
   return (
     <>
-      <div>
-        <h3>Drag and Drop Project Component</h3>
-      </div>
+      <CodeBlock code={code} language={language}>
+        <CodeBlock.Code className='bg-black'>
+          <CodeBlock.LineContent>
+            <CodeBlock.Token />
+          </CodeBlock.LineContent>
+        </CodeBlock.Code>
+      </CodeBlock>
     </>
   );
 }
