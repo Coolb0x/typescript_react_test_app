c72111fc10a40c4a9eae8dabffdd037198d7396a
Coolb0x <45721224+Coolb0x@users.noreply.github.com>
2025-01-06 13:29:46 +0200

    Added React Router
    Added React Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import TestFunctions from "./components/TestFunctions/TestFunctions";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  function Home() {
+    return <h2>Home</h2>;
+  }
+
+  function About() {
+    return <h2>About</h2>;
+  }
+
+  function Users() {
+    return <h2>Users</h2>;
+  }
 
   return (
     <>
@@ -26,6 +39,37 @@ function App() {
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
       <TestFunctions />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
+              <li>
+                <Link to='/users'>Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path='/about'>
+              <About />
+            </Route>
+            <Route path='/users'>
+              <Users />
+            </Route>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
